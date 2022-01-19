7f385909ceb37bbcdb87546edd572193cdadf4c3
Atul <atulkharwal9329@gmail.com>
2022-01-19 22:49:50 +0530

    conflict

--- src/configs/db.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-const mongoose = require("mongoose");
-
-module.exports = () => {
-  return mongoose.connect(
-    "mongodb+srv://atul:1234@cluster0.3ginc.mongodb.net/Deyga_clone"
-  );
-};
